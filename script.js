8153270edb7b1bedb0fce0a20991e017b05837f2
Pietro Zardini <pizardini@gmail.com>
2023-09-18 14:57:14 -0300

    adições de js

--- script.js
+++ script.js
@@ -1,49 +1,81 @@
-// validação de formulário
-
-// const emailInput = document.querySelector('input[name="email"]');
-// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-// function validarEmail() {
-//   if (!emailRegex.test(emailInput.value)) {
-//     alert('Por favor, insira um endereço de e-mail válido.');
-//     emailInput.focus();
-//     return false;
-//   }
-//   return true;
-// }
-
-// const formulario = document.querySelector('form');
-// formulario.addEventListener('submit', (e) => {
-//   if (!validarEmail()) {
-//     e.preventDefault();
-//   }
-// });
-
-//barrinha enchendo
-
-// Função para verificar se um elemento está no campo de visão
-function isElementInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
       rect.top >= 0 &&
       rect.left >= 0 &&
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-  
-  // Função para adicionar a classe 'ativo' à barra de progresso quando estiver visível
-  function animarBarraDeProgresso() {
-    const barrasDeProgresso = document.querySelectorAll('.tecnologias-list-item');
-  
-    barrasDeProgresso.forEach((barra) => {
-      if (isElementInViewport(barra)) {
-        barra.classList.add('ativo');
-      }
-    });
-  }
-  
-  // Adicione um ouvinte de evento de rolagem para chamar a função quando a página rolar
-  window.addEventListener('scroll', animarBarraDeProgresso);
+  );
+}
 
-  animarBarraDeProgresso();
+function animateTechBars() {
+  const techItems = document.querySelectorAll('.tecnologias-list-item');
+
+  techItems.forEach((item) => {
+      if (isElementInViewport(item)) {
+          const barra = item.querySelector('.barra');
+          const level = item.querySelector('.level');
+
+          barra.style.width = level.style.width;
+
+          barra.classList.add('animate-barra');
+      }
+  });
+}
+
+window.addEventListener('scroll', animateTechBars);
+
+animateTechBars();
+
+
+function addRequiredAsterisks() {
+  const requiredFields = document.querySelectorAll('[required]');
+  requiredFields.forEach(field => {
+      const label = document.querySelector(`label[for="${field.id}"]`);
+      if (label) {
+          const asterisk = document.createElement('span');
+          asterisk.className = 'required';
+          asterisk.textContent = ' *';
+          label.appendChild(asterisk);
+      }
+  });
+}
+
+addRequiredAsterisks();
+
+function aplicarCor() {
+  const selectedColor = document.getElementById("colorPicker").value;
+  const barra = document.querySelector('.profile');
+  
+  barra.style.backgroundColor = selectedColor;
+}
+
+document.getElementById('applyColor').addEventListener('click', aplicarCor);
+
+function corPadrao() {
+  const barra = document.querySelector('.profile');
+  
+  barra.style.backgroundColor = "#39383a";
+}
+
+document.getElementById('padrao').addEventListener('click', corPadrao);
+
+// Função para aumentar ou diminuir o tamanho das fontes
+function changeFontSize(value) {
+  const elementsWithFontSize = document.querySelectorAll('body, p, h1, h2, h3, h4, h5, h6, a');
+
+  elementsWithFontSize.forEach(element => {
+    const currentSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+    const newSize = (parseFloat(currentSize) + parseFloat(value)) + 'px';
+    element.style.fontSize = newSize;
+  });
+}
+
+// Seletor para o dropdown
+const fontSizeSelector = document.getElementById('font-size');
+
+// Event listener para detectar a mudança na seleção
+fontSizeSelector.addEventListener('change', function () {
+  const selectedValue = fontSizeSelector.value;
+  changeFontSize(selectedValue);
+});
